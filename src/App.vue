41880ccb3ea4aeaf36a50077e8e65f92c297dbf1
chenlinlin <chenlinlin@threathunter.cn>
2023-10-05 08:04:16 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <initCar />
+  <!-- <initCar /> -->
+  <person />
 </template>
 
 <script setup>
 import { onMounted } from "vue";
 import initCar from "./views/changeCarColor/index.vue";
+import person from './views/person/index.vue';
 </script>
